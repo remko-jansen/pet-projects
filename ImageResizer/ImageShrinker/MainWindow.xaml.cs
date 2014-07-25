e58452a76f30d7a442f1848ced39370eca7efdeb
Remko Jansen <remko.jansen@linkit.nl>
2014-07-25 12:56:45 +0200

    First verion of ImageShrinker added to git

---
+++ ImageResizer/ImageShrinker/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageShrinker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length > 0)
+            {
+                InputImageFile.Text = files[0];
+            }
+        }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+            }
+
+        }
+
+        private void InputImageFile_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var tb = sender as TextBox;
+
+            ImagePreview.Source = null;
+            if (tb != null)
+            {
+                var image = GetImageFromFile(tb.Text);
+                if (image != null)
+                {
+                    ImagePreview.Source = image;
+                }
+            }
+        }
+
+        private BitmapImage GetImageFromFile(string path)
+        {
+            BitmapImage result = null;
+            try
+            {
+                if (File.Exists(path))
+                {
+                    result = new BitmapImage(new Uri(path));
+                }
+            }
+            catch (Exception e)
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+}
